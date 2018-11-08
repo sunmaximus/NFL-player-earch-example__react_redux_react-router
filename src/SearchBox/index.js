31f9cc2ad45bbea107c59e1eedaf1852041df08d
Son Nguyen <sonnguyen@Sons-MBP.fios-router.home>
2018-11-08 15:55:46 -0500

    Search box Component

--- src/SearchBox/index.js
+++ src/SearchBox/index.js
@@ -3,19 +3,34 @@ import React, { Component } from 'react';
 import './searchbox.scss';
 
 class SearchBox extends Component {
+  renderResults() {
+    return this.props.results && this.props.results.slice(0, 10).map(
+      player => (
+        <div 
+          key={player.PlayerID}
+          className='searchbox__results-content'
+        >
+          {`${player.FirstName} ${player.LastName}`}
+        </div>
+      )
+    )
+  }
+
   render() {
+    const { onSearch } = this.props;
+    console.log(this.props.results)
     return (
       <div className='searchbox__container'>
         <input
           className='searchbox__input'
-          onChange={(event) => console.log(event.target.value) }
+          onChange={(event) => onSearch && onSearch(event.target.value) }
           placeholder={'search player...'}
         />
 
         <div className='searchbox__results'>
+          {this.renderResults()}
         </div>
       </div>
-
     );
   }
 }

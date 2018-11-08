e37f5b976ee04d79be09b0b8d53bea9aa6ab2bfd
Son Nguyen <sonnguyen@Sons-MBP.fios-router.home>
2018-11-07 21:49:31 -0500

    Add Redux

---
+++ src/SearchBox/index.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+import './searchbox.scss';
+
+class SearchBox extends Component {
+  render() {
+    return (
+      <div>Test</div>
+    );
+  }
+}
+
+export default SearchBox;

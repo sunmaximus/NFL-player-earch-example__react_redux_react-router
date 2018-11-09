fd2792090d57bfc7cceabb9e43273c2d785d1ef3
Son Nguyen <sonnguyen@Sons-MBP.fios-router.home>
2018-11-09 00:20:10 -0500

    Updates

---
+++ precache-manifest.2a0a45452baf147f6c5f9830307e3db0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "b3581732129f6a2cae18",
+    "url": "/sport-search/static/js/runtime~main.b3581732.js"
+  },
+  {
+    "revision": "827a2df6927f45af3314",
+    "url": "/sport-search/static/js/main.827a2df6.chunk.js"
+  },
+  {
+    "revision": "62cbe993cce3c103a935",
+    "url": "/sport-search/static/js/1.62cbe993.chunk.js"
+  },
+  {
+    "revision": "827a2df6927f45af3314",
+    "url": "/sport-search/static/css/main.c49134f7.chunk.css"
+  },
+  {
+    "revision": "365541dc8c680a33c695827d2a72870b",
+    "url": "/sport-search/index.html"
+  }
+];
---
+++ service-worker.js
@@ -0,0 +1,34 @@
+/**
+ * Welcome to your Workbox-powered service worker!
+ *
+ * You'll need to register this file in your web app and you should
+ * disable HTTP caching for this file too.
+ * See https://goo.gl/nhQhGp
+ *
+ * The rest of the code is auto-generated. Please don't update this file
+ * directly; instead, make changes to your Workbox build configuration
+ * and re-run your build process.
+ * See https://goo.gl/2aRDsh
+ */
+
+importScripts("https://storage.googleapis.com/workbox-cdn/releases/3.6.3/workbox-sw.js");
+
+importScripts(
+  "/sport-search/precache-manifest.2a0a45452baf147f6c5f9830307e3db0.js"
+);
+
+workbox.clientsClaim();
+
+/**
+ * The workboxSW.precacheAndRoute() method efficiently caches and responds to
+ * requests for URLs in the manifest.
+ * See https://goo.gl/S9QRab
+ */
+self.__precacheManifest = [].concat(self.__precacheManifest || []);
+workbox.precaching.suppressWarnings();
+workbox.precaching.precacheAndRoute(self.__precacheManifest, {});
+
+workbox.routing.registerNavigationRoute("/sport-search/index.html", {
+  
+  blacklist: [/^\/_/,/\/[^\/]+\.[^\/]+$/],
+});
